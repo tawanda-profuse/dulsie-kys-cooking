39bfd11d6de0d0519cf5af343b9648ab635b7310
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2025-04-13 07:38:55 +0200

    Create website footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Aclonica, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
       > 
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
