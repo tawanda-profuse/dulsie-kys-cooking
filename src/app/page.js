8de64b6f09430c034e27111fce22751726f7efa4
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2025-04-17 17:39:33 +0200

    Update text in about section

--- src/app/page.js
+++ src/app/page.js
@@ -109,7 +109,7 @@ export default function Home() {
                 target="_blank"
                 href="https://www.herald.co.zw/restaurants-embrace-first-ladys-traditional-foods-call/"
               >
-                Embracing Traditional Dishes
+                ENDORSEMENT BY 1ST LADY-"Embracing Traditional Dishes"
               </a>
             </li>
           </ul>
