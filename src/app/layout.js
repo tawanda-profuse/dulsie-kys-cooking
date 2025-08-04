f0d8fd8630ada62387cf740802b5ebb8e9d1b28d
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2025-08-04 16:25:57 +0200

    Remove unused import

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Aclonica, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
